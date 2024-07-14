57850178135dd4675382920ab4195656bb1d77cb
Luidson Lima Santos <luidsonlsti@gmail.com>
2024-07-13 21:07:37 -0300

    update - runnerz

--- runnerz/backend/src/main/java/com/estudos/runnerz/run/Run.java
+++ runnerz/backend/src/main/java/com/estudos/runnerz/run/Run.java
@@ -1,13 +1,22 @@
 package com.estudos.runnerz.run;
 
+import jakarta.validation.constraints.NotEmpty;
+
 import java.time.LocalDateTime;
 
 public record Run (
     Integer id,
+    @NotEmpty
     String title,
     Coordinate startPoint,
     Coordinate endPoint,
     LocalDateTime startTime,
     LocalDateTime endTime
-){ }
+){
+    public Run {
+        if (endTime != null && !endTime.isAfter(startTime)) {
+            throw new IllegalArgumentException("End time must be after start time");
+        }
+    }
+}
 
---
+++ runnerz/backend/src/main/java/com/estudos/runnerz/run/RunRepository.java
@@ -0,0 +1,58 @@
+package com.estudos.runnerz.run;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+@Repository
+public class RunRepository {
+    private List<Run> runs = new ArrayList<Run>();
+
+    List<Run> findAll(){
+        return runs;
+    }
+
+    Optional<Run> findById(Integer id){
+        return runs.stream().filter(run -> Objects.equals(run.id(), id)).findFirst();
+    }
+
+    void create(Run run){
+        runs.add(run);
+    }
+
+    void update (Run run, Integer id){
+        Optional<Run> existinRun = findById(id);
+
+        existinRun.ifPresent(value -> runs.set(runs.indexOf(value), run));
+    }
+
+    void delete(Integer id){
+        runs.removeIf(run -> run.id().equals(id));
+    }
+
+    @PostConstruct
+    private  void init(){
+        runs.add(new Run(
+                1,
+                "Running in the 90's",
+                new Coordinate(20.3, 20.3),
+                new Coordinate(20.3001, 20.3001),
+                LocalDateTime.now(),
+                LocalDateTime.now().plusHours(1)
+        ));
+        runs.add(new Run(
+                2,
+                "Running alone",
+                new Coordinate(30.3, 20.3),
+                new Coordinate(30.3002, 20.3001),
+                LocalDateTime.now(),
+                LocalDateTime.now().plusHours(2)
+
+        ));
+    }
+}
